5c53033427323ce2768a15123fcec4583549744d
Dan Gibbons <dan.gibbons@wiz.io>
2023-11-01 14:48:31 +0000

    Update bucket due to owner updates

--- s3_audit_access_logs.tf
+++ s3_audit_access_logs.tf
@@ -13,21 +13,29 @@ module "audit_access_s3_label" {
 resource "aws_s3_bucket" "audit_access_logs" {
   count         = var.audit_access_logs_create ? 1 : 0
   bucket        = module.audit_access_s3_label.id
-  acl           = "log-delivery-write"
   force_destroy = false
-  #policy        = "${var.policy}"
-
-    server_side_encryption_configuration {
-        rule {
-        apply_server_side_encryption_by_default {
-            kms_master_key_id = var.kms_key_id
-            sse_algorithm     = var.kms_key_id == "" ? "AES256" : "aws:kms"
-        }
-        }
-    }
 
   tags = merge(tomap({ "Name" = var.audit_s3_access_bucket_name}), var.tags)
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "audit_access_logs" {
+  bucket = aws_s3_bucket.audit_access_logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+        kms_master_key_id = var.kms_key_id
+        sse_algorithm     = var.kms_key_id == "" ? "AES256" : "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "audit_access_logs" {
+  bucket = aws_s3_bucket.audit_access_logs.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
 
 resource "aws_s3_bucket_public_access_block" "audit_s3_access" {
   count               = var.audit_access_logs_create ? 1 : 0
